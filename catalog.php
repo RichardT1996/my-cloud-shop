8f0e97308f14a3edfb1a6f3f9b8c63d344c53a2d
RichardT1996 <rwthomas96@gmail.com>
2025-11-17 20:56:31 +0000

    button style

--- catalog.php
+++ catalog.php
@@ -106,7 +106,7 @@ sqlsrv_close($conn);
     <div>
       <a href="index.php" style="background:#3498db; margin-right:8px;">Home</a>
       <?php if (isset($_SESSION['user_email']) && $_SESSION['user_email'] === 'admin@gmail.com'): ?>
-        <a href="admin_dashboard.php" style="background:#9b59b6; margin-right:8px;">Manage Products</a>
+        <a href="admin_dashboard.php">Manage Products</a>
       <?php endif; ?>
       <a href="logout.php">Log Out</a>
     </div>
